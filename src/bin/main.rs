8c1e575e0827be8fb54305aa7371c532e17c927e
GRBurst <GRBurst@protonmail.com>
2018-12-31 18:55:57 +0100

    check vulkan accessibility on run

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,14 @@
 #[macro_use]
 extern crate vulkano;
 
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+
 
 fn main(){
 println!("Hello, world!");
+
+let instance = Instance::new(None, &InstanceExtensions::none(), None)
+    .expect("failed to create instance");
 
 }
