bd00345e282d9f9f1b081ef86fee63255e59b899
GRBurst <GRBurst@protonmail.com>
2018-12-31 18:55:28 +0100

    propagate vulkan in nix shell by usign shell hook in default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,11 @@
-{nixpkgs ? import <nixos-unstable> { }, cargo ? nixpkgs.cargo }:
+{nixpkgs ? import <nixos-unstable> { } }:
 
 with nixpkgs;
 
 pkgs.stdenv.mkDerivation {
   name = "rustyvulkan";
-  buildInputs = [ cargo cmake python3 ];
+  buildInputs = [ cargo cmake python3 vulkan-headers vulkan-loader vulkan-tools ];
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.nix-profile/lib/"
+  '';
 }
