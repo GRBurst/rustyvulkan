a1c45f4af46e944b3b330ffdc52c3d8973e6dca7
GRBurst <GRBurst@protonmail.com>
2018-12-31 19:11:40 +0100

    Create device and extract

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,12 +3,27 @@ extern crate vulkano;
 
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
 
+fn main() {
+    println!("Running main!");
 
-fn main(){
-println!("Hello, world!");
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+        .expect("failed to create instance");
 
-let instance = Instance::new(None, &InstanceExtensions::none(), None)
-    .expect("failed to create instance");
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
 
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+                    [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+
+    let queue = queues.next().unwrap();
 }
