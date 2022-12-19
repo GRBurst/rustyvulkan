f48992d64bc3331e8b8c70160f45a379972b8933
GRBurst <GRBurst@protonmail.com>
2022-12-19 21:59:45 +0100

    build: update vulkano to 0.32.3
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,30 +1,18 @@
-#[macro_use]
 extern crate vulkano;
 
-use vulkano::instance::Instance;
-use vulkano::instance::InstanceExtensions;
-use vulkano::instance::PhysicalDevice;
-use vulkano::device::Device;
-use vulkano::device::DeviceExtensions;
-use vulkano::device::Features;
+use vulkano::VulkanLibrary;
+use vulkano::instance::{Instance, InstanceCreateInfo};
 
 fn main() {
     println!("Running main!");
 
-    let instance = Instance::new(None, &InstanceExtensions::none(), None)
-        .expect("failed to create instance");
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let instance = Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
 
-    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
-
-    let queue_family = physical.queue_families()
-        .find(|&q| q.supports_graphics())
-        .expect("couldn't find a graphical queue family");
-
-    let (device, mut queues) = {
-        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
-                    [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
-    };
-
-    let queue = queues.next().unwrap();
+    let physical = instance
+        .enumerate_physical_devices()
+        .expect("could not enumerate devices")
+        .next()
+        .expect("no devices available");
 
 }
