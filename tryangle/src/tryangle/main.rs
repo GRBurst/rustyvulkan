c016e33d00dae33171bcb4626a7900e88ec5a871
GRBurst <GRBurst@protonmail.com>
2024-05-06 21:12:33 +0200

    refactor: put vertex and fragment shader in separate files

--- tryangle/src/tryangle/main.rs
+++ tryangle/src/tryangle/main.rs
@@ -302,30 +302,14 @@ pub fn run() {
     mod vs {
         vulkano_shaders::shader! {
             ty: "vertex",
-            src: "
-				#version 450
-
-				layout(location = 0) in vec2 position;
-
-				void main() {
-					gl_Position = vec4(position, 0.0, 1.0);
-				}
-			"
+            path: "src/shaders/main.vs"
         }
     }
 
     mod fs {
         vulkano_shaders::shader! {
             ty: "fragment",
-            src: "
-				#version 450
-
-				layout(location = 0) out vec4 f_color;
-
-				void main() {
-					f_color = vec4(1.0, 0.0, 0.0, 1.0);
-				}
-			"
+            path: "src/shaders/main.fs"
         }
     }
 
