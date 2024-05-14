0b673e0a80068ac9d36a492b8cb45b2392525374
GRBurst <GRBurst@protonmail.com>
2024-05-14 21:46:25 +0200

    add shell.nix to trypot and explicitly export VK_LAYER_PATH

---
+++ trypot/shell.nix
@@ -0,0 +1,36 @@
+let
+  pkgs = import <nixpkgs> { };
+  pname = "rustyvulkan";
+in
+pkgs.mkShell {
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    fontconfig
+  ];
+
+  buildInputs = with pkgs; [
+    cargo
+    cmake
+    python3
+    vulkan-headers
+    vulkan-loader
+    vulkan-tools
+    vulkan-validation-layers
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+
+    libxkbcommon
+    glslang
+
+    blender
+  ];
+
+  shellHook = ''
+    echo "--- Welcome to ${pname}! ---"
+    export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.nix-profile/lib/:${pkgs.vulkan-loader}/lib:${pkgs.libxkbcommon}/lib:${pkgs.vulkan-validation-layers}/lib"
+  '';
+}
